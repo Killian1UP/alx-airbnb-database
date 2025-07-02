179bb6b3d4edc7a3d33b31e53f4ccac9c579b692
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-02 23:38:34 +0200

    Updated the sql file.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,9 @@
 USE airbnb;
+
 CREATE INDEX idx_property_name_location
 ON property(name, location);
 
 CREATE INDEX idx_booking_user_property
 ON booking(user_id, property_id);
+
+EXPLAIN ANALYZE SELECT * FROM property WHERE name = 'City Loft'
